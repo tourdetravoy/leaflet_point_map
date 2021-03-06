e3b3f1fd68cd3378c381e95e22f776dab0eadd3d
tourdetravoy <dooganb@gmail.com>
2021-03-06 19:36:08 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@
 const dataLocation = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRsoGW7XN4iTODr6o4KUI1KQD_Le7tWINP281kwc2t8ImP-Rym640K-TdJId4Tl9ZEdZP_-obBhPQdF/pub?gid=1046386707&single=true&output=csv';
 
 // Map's initial center, and zoom level
-const mapCenter = [45.65, 1.00];
-const mapZoom = 5;
+const mapCenter = [50.00, 10.00];
+const mapZoom = 3;
 
 // Marker icon height and width
 const iconHeight = 10;
