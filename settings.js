ccce57c3eeb5625f19ac18fd8ea4803e27e69729
tourdetravoy <dooganb@gmail.com>
2021-03-06 19:22:02 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 // Where to load data from. Choose either a local file in GitHub repo, such as `data/places.csv`,
 // or a Google Sheets URL (File > Publish to the Web > CSV )
-const dataLocation = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRsoGW7XN4iTODr6o4KUI1KQD_Le7tWINP281kwc2t8ImP-Rym640K-TdJId4Tl9ZEdZP_-obBhPQdF/pub?output=csv';
+const dataLocation = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRsoGW7XN4iTODr6o4KUI1KQD_Le7tWINP281kwc2t8ImP-Rym640K-TdJId4Tl9ZEdZP_-obBhPQdF/pub?gid=1046386707&single=true&output=csv';
 
 // Map's initial center, and zoom level
-const mapCenter = [41.65, -72.7];
+const mapCenter = [45.65, 1.00];
 const mapZoom = 7;
 
 // Marker icon height and width
